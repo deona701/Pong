e7b08bacb8b4894257b4d9609212bd4fd75aa60d
deona701 <ddeonaabazi@gmail.com>
2026-09-11 19:32:50 +0200

    added code to opponentController for movement

--- Assets/Scripts/OpponentController.cs
+++ Assets/Scripts/OpponentController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class OpponentController : MonoBehaviour
 {
+    public float speed = 15.0f;
+    public float yBoundary = 4.0f;
     private Transform ballTransform;
 
     void Start()
@@ -11,6 +13,13 @@ public class OpponentController : MonoBehaviour
 
     void Update()
     {
-        
+        if (ballTransform.position.y > transform.position.y) {
+            float newY = transform.position.y + (speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(newY, -yBoundary, yBoundary), transform.position.z);
+        }
+        else if (ballTransform.position.y < transform.position.y) {
+            float newY = transform.position.y - (speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(newY, -yBoundary, yBoundary), transform.position.z);
+        }
     }
 }
