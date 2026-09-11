81dca96c50a5b2f894fd82ad4223f0e7673016c1
deona701 <ddeonaabazi@gmail.com>
2026-09-11 19:16:08 +0200

    added code to ballController

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public Rigidbody2D ballRB;
+    public float directionVertical;
+    public int directionHorizontal;
+
     void Start()
     {
-        
-    }
+        ballRB = GetComponent<Rigidbody2D>();
+        directionVertical = Random.Range(-4.0f, 4.0f);
+        directionHorizontal = Random.Range(0, 2);
 
-    void Update()
-    {
-        
+        if (directionHorizontal == 0) {
+            ballRB.linearVelocity = new Vector2(5f, directionVertical);
+        }
+        else {
+            ballRB.linearVelocity = new Vector2(-5f, directionVertical);
+        }
     }
 }
