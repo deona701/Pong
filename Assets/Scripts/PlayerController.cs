41a57c8b237e362cbfcb20dd1175d45c618bd315
deona701 <ddeonaabazi@gmail.com>
2026-09-11 18:49:52 +0200

    added code to playercontroller script for player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public float speed = 15.0f;
+    public float yBoundary = 4.0f;
 
     void Update()
     {
-        
+        float moveInput = Input.GetAxisRaw("Vertical");
+        float newY = transform.position.y + (moveInput * speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(newY, -yBoundary, yBoundary), transform.position.z);
     }
 }
